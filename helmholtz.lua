cb3647c54e272458800906bffdc63b5db8795de9
Julien <julien@deepmind.com>
2012-11-26 19:55:23 +0000

    Code Wake-Sleep algorithm :) TODO: now test it on the tutorial example

--- helmholtz.lua
+++ helmholtz.lua
@@ -5,10 +5,26 @@
 require 'torch'
 require 'dok'
 
+
+function Sigmoid(x) 
+   return x:apply(function(x) return 1/(1+math.exp(-x)) end)
+--   x:mul(-1):exp():add(1):pow(-1)
+--  return x
+end
+
+function ExtendColumnByOne(x) 
+   assert(x:size(2) == 1, 'not a column vector')
+   x:resize(x:nElement()+1,1)
+   x[x:nElement()] = 1
+   return x
+end
+
 do 
    local Helmholtz = torch.class('Helmholtz')
 
    function Helmholtz:__init()
+      -- learning step
+      self.step = 0.1
       -- dimensions of the layers
       self.nx = 1
       self.ny = 6
@@ -22,8 +38,52 @@ do
       self.VR = torch.zeros(self.ny, self.nd+1)
    end
 
-	function Helmholtz:Wake(data)
-	 	 
+	function Helmholtz:Wake(d, step)
+      step = step or self.step
+      -- Experience reality!
+      local d = ExtendColumnByOne(d)
+      -- Pass sense datum up through recognition network
+      local y = ExtendColumnByOne(
+                Sigmoid(torch.mm(self.VR, d)):apply(torch.bernoulli)
+            )
+      local x = ExtendColumnByOne(
+               Sigmoid(torch.mm(self.WR, y)):apply(torch.bernoulli)
+            )
+
+      -- Pass back down through generation network, saving computer probabilities
+      local xi = Sigmoid(self.bG:clone())
+      local psi = Sigmoid(torch.mm(self.WG, x))
+      local delta = Sigmoid(torch.mm(self.VG, y))
+
+      -- Adjust generative weights by delta rule 
+      self.bG:add(step, x[{{1,self.nx}}] - xi)
+      self.WG:addmm(step, (y[{{1,self.ny}}] - psi), x:t())
+      self.VG:addmm(step, (d[{{1,self.nd}}] - delta), y:t())
 	end
+
+	function Helmholtz:Sleep(step)
+      step = step or self.step
+      -- Initiate a dream!
+      local x = ExtendColumnByOne(
+               Sigmoid(self.bG:clone()):apply(torch.bernoulli)
+            )
+
+      -- Pass dream signal down through generation network 
+      local y = ExtendColumnByOne(
+                Sigmoid(torch.mm(self.WG, x)):apply(torch.bernoulli)
+            )
+      local d = ExtendColumnByOne(
+               Sigmoid(torch.mm(self.VG, y)):apply(torch.bernoulli)
+            )
+
+      -- Pass back up through recognition network, saving computer probabilities
+      local psi = Sigmoid(torch.mm(self.VR, d))
+      local xi = Sigmoid(torch.mm(self.WR, y))
+
+      -- Adjust generative weights by delta rule 
+      self.VR:addmm(step, (y[{{1,self.ny}}] - psi), d:t())
+      self.WR:addmm(step, (x[{{1,self.nx}}] - xi), y:t())
+	end
+
 
 end
--- test/test.lua
+++ test/test.lua
@@ -20,10 +20,49 @@ function mytest.TestInitialize()
 	tester:asserteq(h, nil, "h shouldn't exist")
 end
 
+function mytest.TestSigmoid()
+   local v = {-1, 0, 1}
+   local x = torch.Tensor(v)
+   local y = Sigmoid(x)
+   tester:asserteq(y, x, 'did not return the tensor')
+   for i=1,#v do
+      tester:assertne(x[i], v[i],'did not modify tensor')
+      tester:asserteq(x[i], 1/(1+math.exp(-v[i])),'did not compute a sigmoid')
+   end
+end
+
+function mytest.TestExtendColumnByOne()
+   local v = {10, 11, 12}
+   local x = torch.Tensor({v}):t()
+   tester:asserteq(x:nElement(), 3, 'wrong size before')
+   tester:asserteq(x:size(1), 3, 'wrong number of rows')
+   tester:asserteq(x:size(2), 1, 'wrong number of columns')
+   local y = ExtendColumnByOne(x)
+   tester:asserteq(y, x, 'did not return the tensor')
+   tester:asserteq(x:nElement(), 4, 'wrong size after')
+   tester:asserteq(x[{4,1}], 1, 'not ending by 1')
+   tester:asserteq(x[4][1], 1, 'not ending by 1')
+end
+
 function mytest.TestWake()
 	local h = Helmholtz()
-  	local d = torch.zeros(1,9)
+  	local d = torch.zeros(9,1)
+   local oldBG = h.bG:clone()
+   local oldVG = h.VG:clone()
+   local oldWG = h.WG:clone()
 	h:Wake(d)
+   tester:assertge((oldBG-h.bG):abs():max(), 1e-16, "h.bG has not changed") 
+   tester:assertge((oldVG-h.VG):abs():max(), 1e-16, "h.VG has not changed") 
+   tester:assertge((oldWG-h.WG):abs():max(), 1e-16, "h.WG has not changed") 
+end
+
+function mytest.TestSleep()
+	local h = Helmholtz()
+   local oldVR = h.VR:clone()
+   local oldWR = h.WR:clone()
+	h:Sleep()
+   tester:assertge((oldVR-h.VR):abs():max(), 1e-16, "h.VR has not changed") 
+   tester:assertge((oldWR-h.WR):abs():max(), 1e-16, "h.VR has not changed") 
 end
  
 function mytest.TestFail()
