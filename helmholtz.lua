f65ce1a0fc676162b07e3373599e082ab220a456
Julien <julien@deepmind.com>
2012-12-03 18:56:34 +0000

    Start exact computation

--- helmholtz.lua
+++ helmholtz.lua
@@ -8,21 +8,23 @@ require 'dok'
 do
    function Sigmoid(x)
     --  return x:apply(function(x) return 1/(1+math.exp(-x)) end)
-      x:mul(-1):exp():add(1):pow(-1)
-      return x
+      local y = x:clone()
+      y:mul(-1):exp():add(1):pow(-1)
+      return y
    end
 
    function ShrinkColumnByOne(x)
       assert(x:size(2) == 1, 'not a column vector')
-      x:resize(x:nElement()-1,1)
-      return x
+      local y = x:clone()
+      return y:resize(y:nElement()-1,1)
    end
 
    function ExtendColumnByOne(x)
       assert(x:size(2) == 1, 'not a column vector')
-      x:resize(x:nElement()+1,1)
-      x[x:nElement()] = 1
-      return x
+      local y = x:clone()
+      y:resize(y:nElement()+1,1)
+      y[y:nElement()] = 1
+      return y
    end
 
    local Helmholtz = torch.class('Helmholtz')
@@ -48,18 +50,31 @@ do
       self.VR = torch.zeros(self.ny, self.nd+1)
    end
 
-   function Helmholtz:Wake(d )
+   function Helmholtz:RecognizeExtended(d, saveproba)
+      local samplefunction
+      if saveproba then
+         samplefunction = function(p) return p end
+      else
+         samplefunction = torch.bernoulli
+      end
+
       -- Experience reality!
       local d = ExtendColumnByOne(d)
       -- Pass sense datum up through recognition network
       local y = ExtendColumnByOne(
-                Sigmoid(torch.mm(self.VR, d)):apply(torch.bernoulli)
+                Sigmoid(torch.mm(self.VR, d)):apply(samplefunction)
             )
       local x = ExtendColumnByOne(
-               Sigmoid(torch.mm(self.WR, y)):apply(torch.bernoulli)
+               Sigmoid(torch.mm(self.WR, y)):apply(samplefunction)
             )
+      return y, x
+   end
 
-      -- Pass back down through generation network, saving computer probabilities
+   function Helmholtz:Wake(d)
+      -- Experience reality!
+      local y, x = self:RecognizeExtended(d, self.exact)
+
+      -- Pass back down through generation network, saving computed probabilities
       local xi = Sigmoid(self.bG:clone())
       local psi = Sigmoid(torch.mm(self.WG, x))
       local delta = Sigmoid(torch.mm(self.VG, y))
@@ -70,11 +85,11 @@ do
       self.VG:addmm(self.stepV, (d[{{1,self.nd}}] - delta), y:t())
    end
 
-   function Helmholtz:SampleExtended()
+   function Helmholtz:GenerateExtended(saveproba)
       -- Initiate a dream!
-      local x = ExtendColumnByOne(
-               Sigmoid(self.bG:clone()):apply(torch.bernoulli)
-            )
+      local x = Sigmoid(self.bG:clone())
+      if not saveproba then x:apply(torch.bernoulli) end
+      x = ExtendColumnByOne(x)
 
       -- Pass dream signal down through generation network 
       local y = ExtendColumnByOne(
@@ -87,13 +102,13 @@ do
       return d, y, x
    end
 
-   function Helmholtz:Sample()
-      d, y, x = self:SampleExtended()
+   function Helmholtz:Generate()
+      d, y, x = self:GenerateExtended()
       return ShrinkColumnByOne(d), ShrinkColumnByOne(y), ShrinkColumnByOne(x)
    end
 
    function Helmholtz:Sleep()
-      local d, y, x = self:SampleExtended()
+      local d, y, x = self:GenerateExtended(self.exact)
 
       -- Pass back up through recognition network, saving computer probabilities
       local psi = Sigmoid(torch.mm(self.VR, d))
